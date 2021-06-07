8892b50a7f3b2295e643a535795bf1324696cf35
umberto10 <umberto10@o2.pl>
2021-06-07 10:20:14 +0200

    small fix

--- src/mount_one.sh
+++ src/mount_one.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 TOKEN=$(<"${HOME}"/token)
-oneclient -v 1 -H onedata.kdm.wcss.pl -t "$TOKEN" "${HOME}"/onedata
+echo ${TOKEN}
+oneclient -v 1 -H onedata.kdm.wcss.pl -t ${TOKEN} "${HOME}"/onedata
